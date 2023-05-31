782dc3bdb4856fcef0bb71a92913f46beabe28de
codeYuzen <joao222martins@gmail.com>
2023-05-31 20:07:59 +0100

    update layout e js

---
+++ Front-end/js/index.js
@@ -0,0 +1,29 @@
+function addRow() {
+    var img = document.getElementById('image').value;
+    var name = document.getElementById('name').value;
+    var category = document.getElementById('category').value;
+    var description = document.getElementById('description').value;
+    var warn = '';
+
+    if (img.length < 3)
+        warn += 'Image is invalid \n';
+    if (name.length < 3)
+        warn += 'Name is invalid \n';
+    if (category.length < 3)
+        warn += 'Category is invalid';
+     if (description.length < 3)
+        warn += 'Description is invalid';
+
+    if (warn != '')
+        alert(warn)
+
+    else {
+        document.getElementById('tb').innerHTML += '<tr>' + '<td>' + img + '</b>' + '<td>' + name + '</b>' +
+            '<td>' + category + '</b>' + '<td>' + img + '</b>' + '<td>' + '</tr>'
+
+
+    }
+
+
+
+}
