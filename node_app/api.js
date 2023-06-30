4c73afb82c1ec3e7a387599d22525c9a87a15d20
D34dlyK1ss <rags.ftjs@gmail.com>
2023-06-30 15:07:13 +0100

    admin minimal functions

--- node_app/api.js
+++ node_app/api.js
@@ -1,37 +1,39 @@
-const express = require('express')
-var cors = require('cors')
-var app = express()
-var dbRepo = require('./db-repo').databaseRepo();
+const express = require("express");
+let cors = require("cors");
+let app = express();
+let dbRepo = require("./db-repo").databaseRepo();
 
-const bodyParser = require('body-parser');
-const port = 3000
+const bodyParser = require("body-parser");
+const port = 3000;
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/addComponent', (req, res) => {
+app.post("/addComponent", (req, res) => {
   let data = req.body;
-  let insertedComponent = dbRepo.insert(data)
+  let insertedComponent = dbRepo.insert(data);
+
   res.send(insertedComponent);
 });
 
-app.post('/updateComponent', (req, res) => {
+app.post("/updateComponent", (req, res) => {
   let data = req.body;
   let updated = dbRepo.update(data);
+
   res.send(updated);
 });
 
-app.post('/deleteComponent', (req, res) => {
-    let id = req.body.id;
+app.post("/deleteComponent", (req, res) => {
+  let id = req.body.id;
+  let result = dbRepo.delete(id);
 
-    var result = dbRepo.delete(id)
-    res.send(result ? 'OK' : 'NOK');
+  res.send(result ? "OK" : "NOK");
 });
 
-app.get('/components', (req, res) => {
-  res.send(dbRepo.list())
+app.get("/components", (req, res) => {
+  res.send(dbRepo.list());
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
--- node_app/db-repo.js
+++ node_app/db-repo.js
@@ -1,56 +1,59 @@
-const fs = require('fs');
+const fs = require("fs");
 
-exports.databaseRepo = function() {
-	let dbName = 'database.json';
-	let readDb = function() {
-		let rawData = fs.readFileSync(dbName);
+exports.databaseRepo = function () {
+  let dbName = "database.json";
+  let readDb = function () {
+    let rawData = fs.readFileSync(dbName);
 
-		return JSON.parse(rawData);
-	};
+    return JSON.parse(rawData);
+  };
 
-	return {
-		update: function(component) {     
-			let dataJson = readDb();
-			let componentToUpdate = dataJson.components.find(x => x.Id === component.Id);
+  return {
+    update: function (component) {
+      let dataJson = readDb();
+      let componentToUpdate = dataJson.components.find((x) => x.id === component.id);
 
-			componentToUpdate.serial = component.serial;
-			componentToUpdate.price = component.price;
-			componentToUpdate.category = component.category;
-			componentToUpdate.manufacturer = component.manufacturer;
-			componentToUpdate.model = component.model;
-			componentToUpdate.color = component.color;
-			componentToUpdate.photo = component.photo;
-			
-			fs.writeFileSync(dbName, JSON.stringify(dataJson, null, '\t'));
+      componentToUpdate.serial = component.serial;
+      componentToUpdate.price = component.price;
+      componentToUpdate.category = component.category;
+      componentToUpdate.manufacturer = component.manufacturer;
+      componentToUpdate.model = component.model;
+      componentToUpdate.color = component.color;
+      componentToUpdate.photo = component.photo;
 
-			return componentToUpdate;
-		},
+      fs.writeFileSync(dbName, JSON.stringify(dataJson, null, "\t"));
 
-		insert: function(component) {
-			let dataJson = readDb();
-			let nextId = dataJson.lastId++;
+      return componentToUpdate;
+    },
 
-			component.Id = nextId;
-			dataJson.components.push(component);
-			fs.writeFileSync(dbName, JSON.stringify(dataJson, null, '\t'));
-			
-			dataJson.lastId = nextId;
+    insert: function (component) {
+      let dataJson = readDb();
+      let nextId = 0;
 
-			return component;
-		},
+      for (const c of dataJson.components) {
+        if (nextId <= c.id) nextId = c.id + 1;
+      }
 
-		list: function() {
-			return readDb().components;
-		},
+      component.id = nextId;
 
-		delete: function(id) {
-			let dataJson = readDb();
+      dataJson.components.push(component);
+      fs.writeFileSync(dbName, JSON.stringify(dataJson, null, "\t"));
 
-			dataJson.components = dataJson.components.filter(x => x.Id != id);
+      return component;
+    },
 
-			fs.writeFileSync(dbName, JSON.stringify(dataJson, null, '\t'));
+    list: function () {
+      return readDb().components;
+    },
 
-			return true;
-		}
-	}
+    delete: function (id) {
+      let dataJson = readDb();
+
+      dataJson.components = dataJson.components.filter((x) => x.id != id);
+
+      fs.writeFileSync(dbName, JSON.stringify(dataJson, null, "\t"));
+
+      return true;
+    },
+  };
 };
